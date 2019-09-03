6640fab096d18600fdfb5292fa376044110b240b
cjhong <cjhong6@gmail.com>
2019-09-02 23:01:24 -0700

    get very specific data from the HTML

--- scarper.py
+++ scarper.py
@@ -3,7 +3,14 @@ from bs4 import BeautifulSoup
 
 html = urlopen('https://treehouse-projects.github.io/horse-land/index.html')
 soup = BeautifulSoup(html.read(), 'html.parser')
-divs = soup.find_all('div',{'class':'featured'})
 
-for div in divs:
-  print(div)
+#get the div tag -> class=featured -> h2 -> text
+# feature_header = soup.find('div',{'class':'featured'}).h2
+# print(feature_header.get_text())
+
+#search for specific css class use attrs
+# for button in soup.find(class_ = 'button button--primary'):
+#   print(button)
+
+for link in soup.find_all('a'):
+  print(link.get('href'))
