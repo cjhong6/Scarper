450415dcf23745a359864d07632ec41ab630cb84
cjhong <cjhong6@gmail.com>
2019-09-02 22:30:56 -0700

    initalized beautifulsoup

---
+++ scarper.py
@@ -0,0 +1,9 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen('https://treehouse-projects.github.io/horse-land/index.html')
+soup = BeautifulSoup(html.read(), 'html.parser')
+divs = soup.find_all('div',{'class':'featured'})
+
+for div in divs:
+  print(div)
